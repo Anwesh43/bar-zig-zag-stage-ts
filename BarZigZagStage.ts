5e652ccd2c4baf3d8595e721563211c8976c0c6b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-03 19:34:22 +0530

    adding drawBZZNode canvas extension methods

--- BarZigZagStage.ts
+++ BarZigZagStage.ts
@@ -27,3 +27,26 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
     return mirrorValue(scale, a, b) * dir * scGap
 }
+
+
+const drawBZZNode : Function = (context : CanvasRenderingContext2D, i : number, scale : number) => {
+    const gap : number = h / (nodes + 1)
+    const size : number = gap / sizeFactor
+    const sc1 : number = divideScale(scale, 0, 2)
+    const sc2 : number = divideScale(scale, 1, 2)
+    const hGap : number = (2 * size) / (bars + 1)
+    context.save()
+    context.translate(w / 2, gap * (i + 1))
+    context.rotate(Math.PI/2 * sc2)
+    context.fillStyle = foreColor
+    context.fillRect(-size, -hGap, size, hGap)
+    context.translate(0, -size)
+    for (var j = 0; j < bars; j++) {
+        const sc : number = divideScale(sc1, j, bars)
+        context.save()
+        context.translate(size * (1 - 2 * j) * sc, -hGap + j * 2 * hGap)
+        context.fillRect(0, 0, size, hGap)
+        context.restore()
+    }
+    context.restore()
+}
