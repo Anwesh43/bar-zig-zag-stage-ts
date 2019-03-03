7e690e77088f28ab9cb17d77bca8fda8db83a871
Anwesh43 <anweshthecool0@gmail.com>
2019-03-03 19:53:58 +0530

    adding BarZigZag class

--- BarZigZagStage.ts
+++ BarZigZagStage.ts
@@ -167,3 +167,26 @@ class BZZNode {
         return this
     }
 }
+
+class BarZigZag {
+    root : BZZNode = new BZZNode(0)
+    curr : BZZNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
